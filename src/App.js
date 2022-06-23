6c3f1b8100b96518fe9d6ebcdb70fe34b25bcd75
khanhldgcd191071 <khanhldgcd191071@fpt.edu.vn>
2022-06-23 21:13:30 +0700

    Build some basic parts of the Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
 
--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,9 +1,54 @@
 import React from "react";
+import { Container, Button, Nav, Navbar } from "react-bootstrap";
 import "./navbar.css"
 
 export default function NavigationBar(){
     return(
         <>
+        <Container className="Navbar-wrapper" fixed="top">
+            <Navbar bg="dark" variant="dark" className="Navigationbar" width="100%">
+                <Container className="Navhead">
+                    <Navbar.Brand>ZA-Bookstore</Navbar.Brand>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            <a href="#">Login</a>/<a href="#">Register</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                    <Button bg="dark" variant="dark" className="DarkmodeSwitch">Darkmode</Button>
+                </Container>
+                <Container>
+                    <Nav bg="dark" variant="dark" className="justify-content-start Category-wrapper">
+                        <Nav.Item>
+                            <Nav.Link href="#">Book</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#">Fiction</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#">Nonfiction</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#">eBooks</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#">Audiobooks</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#">Teen</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#">Kids</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className="justify-content-end">
+                            <Nav.Link href="#">Your Library</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </Container>
+        {/* <br/> */}
+        
+        
         </>
     )
 }
