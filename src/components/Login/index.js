db758c016b6b4dc6bca2590ebce8a6b8b5a08d4f
khanhldgcd191071 <khanhldgcd191071@fpt.edu.vn>
2022-07-20 06:09:38 +0700

    setup login function

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import {  Button, Container, Form, Modal } from "react-bootstrap";
 import "./login.css"
 
 export default function LoginForm(props){
+    const [loginState, setLoginState] = useState({
+        username:"",
+        password:"",
+        saveLoginState: false
+    })
     return(
         <>
         <Modal
@@ -18,18 +23,43 @@ export default function LoginForm(props){
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
+                {JSON.stringify(loginState)}
                 <Container className="loginWrapper">
                     <Form className="loginForm">
                         <Form.Group className="mb-3" controlId="formLoginUsername">
                             <Form.Label>Username:</Form.Label>
-                            <Form.Control type="username" size="lg" placeholder="Enter your Username"/>
+                            <Form.Control 
+                                type="username" 
+                                size="lg" 
+                                placeholder="Enter your Username"
+                                onChange={(event) =>{
+                                    const value = event.target.value;
+                                    setLoginState({...loginState, username:value})
+                                }}
+                            />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formLoginPassword">
                             <Form.Label>Password:</Form.Label>
-                            <Form.Control type="password" size="lg" placeholder="Enter Password"/>
+                            <Form.Control 
+                                type="password" 
+                                size="lg" 
+                                placeholder="Enter Password"
+                                onChange={(event) =>{
+                                    const value = event.target.value;
+                                    setLoginState({...loginState, password:value})
+                                }}
+                            />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="checkboxLoginKeeplogin">
-                            <Form.Check type="checkbox" size="lg" label="Keep login"/>
+                            <Form.Check 
+                                type="checkbox" 
+                                size="lg" 
+                                label="Keep login"
+                                onChange={(event) =>{
+                                    const value = event.target.checked;
+                                    setLoginState({...loginState, saveLoginState:value})
+                                }}
+                            />
                         </Form.Group>
                         <Button variant="primary" size="lg" type="summit">Login</Button>
                     </Form>
