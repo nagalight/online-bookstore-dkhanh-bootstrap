11defd87e617fe67c5665193173697cc4ce142bc
khanhldgcd191071 <khanhldgcd191071@fpt.edu.vn>
2022-07-20 06:10:59 +0700

    setup register function

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button, Container, Form, Modal } from "react-bootstrap";
 import "./register.css"
 
 export default function RegisterForm(props){
+    const [username, setUsername] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [confirmPassword, setConfirmPassword] = useState(null);
+    const [registerCheckbox, setRegisterCheckbox] = useState(false);
+
+    const handleInputChange = (e) => {
+        const {id , value} = e.target;
+        if(id === "username"){
+            setUsername(value);
+        }
+        if(id === "email"){
+            setEmail(value);
+        }
+        if(id === "password"){
+            setPassword(value);
+        }
+        if(id === "confirmPassword"){
+            setConfirmPassword(value);
+        }
+        if(id === "registerCheckbox"){
+            setRegisterCheckbox(value);
+        }
+    }
+
+    const handleSubmit  = () => {
+        console.log(username,email,password,confirmPassword,registerCheckbox);
+    }
     return(
         <>
         <Modal
@@ -20,26 +48,56 @@ export default function RegisterForm(props){
             <Modal.Body>
                 <Container className="registerWrapper">
                     <Form className="registerForm">
-                        <Form.Group className="mb-3" controlId="formRegisterUsername">
+                        <Form.Group className="mb-3" >
                             <Form.Label>Username:</Form.Label>
-                            <Form.Control type="username" size="lg" placeholder="Enter your Username"/>
+                            <Form.Control 
+                                type="username" 
+                                size="lg" 
+                                id="username"
+                                placeholder="Enter your Username"
+                                onChange = {(e) => handleInputChange(e)}
+                            />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formRegisterEmail">
+                        <Form.Group className="mb-3" >
                             <Form.Label>Email:</Form.Label>
-                            <Form.Control type="email" size="lg" placeholder="Enter your Email"/>
+                            <Form.Control 
+                                type="email" 
+                                size="lg" 
+                                id="email" 
+                                placeholder="Enter your Email"
+                                onChange = {(e) => handleInputChange(e)}
+                            />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formRegisterPassword">
+                        <Form.Group className="mb-3" >
                             <Form.Label>Password:</Form.Label>
-                            <Form.Control type="password" size="lg" placeholder="Enter Password"/>
+                            <Form.Control 
+                                type="password" 
+                                size="lg" 
+                                id="password" 
+                                placeholder="Enter Password"
+                                onChange = {(e) => handleInputChange(e)}
+                            />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formRegisterReEnterPassword">
-                            <Form.Label>Re-enter Password:</Form.Label>
-                            <Form.Control type="password" size="lg" placeholder="Re-Enter Password"/>
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Confirm Password:</Form.Label>
+                            <Form.Control 
+                                type="password" 
+                                size="lg" 
+                                id="confirmPassword" 
+                                placeholder="Confirm Password"
+                                onChange = {(e) => handleInputChange(e)}
+                            />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="checkboxRegister">
-                            <Form.Check type="checkbox" size="lg" label="You are agree to our Term of service"/>
+                        <Form.Group className="mb-3" >
+                            <Form.Check 
+                                type="checkbox" 
+                                size="lg" 
+                                id="registerCheckbox" 
+                                label="You are agree to our Term of service"
+                                onChange = {(e) => handleInputChange(e)}
+                            />
                         </Form.Group>
-                        <Button variant="primary" size="lg" type="summit">Register</Button>
+                        <Button variant="primary" size="lg"  onClick={()=>handleSubmit()}>Register</Button>
                     </Form>
                 </Container>
             </Modal.Body>
