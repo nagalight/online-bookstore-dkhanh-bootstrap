6d03111e8965060933a454dc4df43662b93341e4
khanhldgcd191071 <khanhldgcd191071@fpt.edu.vn>
2022-07-20 12:03:17 +0700

    adding validation to register

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -12,38 +12,86 @@ export default function RegisterForm(props){
     const [confirmPassword, setConfirmPassword] = useState(null);
     const [registerCheckbox, setRegisterCheckbox] = useState(false);
 
-    const handleInputChange = (e) => {
-        const {id , value} = e.target;
-        if(id === "username"){
-            setUsername(value);
-        }
-        if(id === "email"){
-            setEmail(value);
-        }
-        if(id === "password"){
-            setPassword(value);
-        }
-        if(id === "confirmPassword"){
-            setConfirmPassword(value);
-        }
-        if(id === "registerCheckbox"){
-            setRegisterCheckbox(value);
-        }
-    }
-
     const handleSubmit  = () => {
-        console.log(username,email,password,confirmPassword);
+        // console.log(registerInput.username,registerInput.email,registerInput.password ,registerInput.confirmPassword);
         let obj = {
-            username : username,
-            email : email,
-            password : password,
-            confirmPassword : confirmPassword,
+            username : registerInput.username,
+            email : registerInput.email,
+            password : registerInput.password,
+            confirmPassword : registerInput.confirmPassword,
         }
         const newPostKey = push(child(ref(database), 'posts')).key;
         const updates = {};
         updates['/' + newPostKey] = obj
         return update(ref(database), updates);
     }
+
+    const [registerInput, setRegisterInput] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+    const [registerError, setRegisterError] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const onRegisterInputChange = e => {
+        const { id, value } = e.target;
+        setRegisterInput(prev => ({
+            ...prev,
+            [id]: value
+          }));
+          console.log(value)
+          validateRegisterInput(e);
+    }
+
+    const validateRegisterInput = e =>{
+        let { id,value } = e.target;
+        setRegisterError(prev => {
+            const stateObj = { ...prev, [id]: "" };
+         
+            switch (id) {
+                case "username":
+                    if (!value) {
+                        stateObj[id] = "Please enter Username.";
+                    }
+                    break;
+                
+                case "email":
+                    if(!value) {
+                        stateObj[id] = "Please enter Email.";
+                    }
+         
+                case "password":
+                    if (!value) {
+                        stateObj[id] = "Please enter Password.";
+                    } else if (registerInput.confirmPassword && value !== registerInput.confirmPassword) {
+                        stateObj["confirmPassword"] = "Password and Confirm Password does not match.";
+                    } else {
+                        stateObj["confirmPassword"] = registerInput.confirmPassword ? "" : registerError.confirmPassword;
+                    }
+                    break;
+         
+                case "confirmPassword":
+                    if (!value) {
+                        stateObj[id] = "Please enter Confirm Password.";
+                    } else if (registerInput.password && value !== registerInput.password) {
+                        stateObj[id] = "Password and Confirm Password does not match.";
+                    }
+                    break;
+         
+                default:
+                    break;
+            }
+         
+            return stateObj;
+        });
+    }
+
     return(
         <>
         <Modal
@@ -67,9 +115,12 @@ export default function RegisterForm(props){
                                 type="username" 
                                 size="lg" 
                                 id="username"
+                                value={registerInput.username}
                                 placeholder="Enter your Username"
-                                onChange = {(e) => handleInputChange(e)}
+                                onChange={onRegisterInputChange}
+                                onBlur={validateRegisterInput}
                             />
+                            {registerError.username && <span className='register_err'>{registerError.username}</span>}
                         </Form.Group>
                         <Form.Group className="mb-3" >
                             <Form.Label>Email:</Form.Label>
@@ -77,29 +128,38 @@ export default function RegisterForm(props){
                                 type="email" 
                                 size="lg" 
                                 id="email" 
+                                value={registerInput.email}
                                 placeholder="Enter your Email"
-                                onChange = {(e) => handleInputChange(e)}
+                                onChange={onRegisterInputChange}
+                                onBlur={validateRegisterInput}
                             />
+                            {registerError.email && <span className='register_err'>{registerError.email}</span>}
                         </Form.Group>
                         <Form.Group className="mb-3" >
                             <Form.Label>Password:</Form.Label>
                             <Form.Control 
                                 type="password" 
                                 size="lg" 
-                                id="password" 
+                                id="password"
+                                value={registerInput.password}
                                 placeholder="Enter Password"
-                                onChange = {(e) => handleInputChange(e)}
+                                onChange={onRegisterInputChange}
+                                onBlur={validateRegisterInput}
                             />
+                            {registerError.password && <span className='register_err'>{registerError.password}</span>}
                         </Form.Group>
                         <Form.Group className="mb-3" >
                             <Form.Label>Confirm Password:</Form.Label>
                             <Form.Control 
                                 type="password" 
                                 size="lg" 
-                                id="confirmPassword" 
+                                id="confirmPassword"
+                                value={registerInput.confirmPassword}
                                 placeholder="Confirm Password"
-                                onChange = {(e) => handleInputChange(e)}
+                                onChange={onRegisterInputChange}
+                                onBlur={validateRegisterInput}
                             />
+                            {registerError.confirmPassword && <span className='register_err'>{registerError.confirmPassword}</span>}
                         </Form.Group>
                         <Form.Group className="mb-3" >
                             <Form.Check 
@@ -107,10 +167,9 @@ export default function RegisterForm(props){
                                 size="lg" 
                                 id="registerCheckbox" 
                                 label="You are agree to our Term of service"
-                                onChange = {(e) => handleInputChange(e)}
                             />
                         </Form.Group>
-                        <Button variant="primary" size="lg"  onClick={()=>handleSubmit()}>Register</Button>
+                        <Button variant="primary" size="lg" onClick={()=>handleSubmit()}>Register</Button>
                     </Form>
                 </Container>
             </Modal.Body>
