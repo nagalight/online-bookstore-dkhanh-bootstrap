7318628a27759ef09b12e89bb6f9feec20c2398a
khanhldgcd191071 <khanhldgcd191071@fpt.edu.vn>
2022-08-26 19:56:55 +0700

    adding Admin accounts table, add admins, users forms

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,12 +1,29 @@
 import React, {useEffect, useState} from "react";
-import { Container, Tab, Tabs, Table, Button } from "react-bootstrap";
+import { Container, Tab, Tabs, Table, Button, Form, Modal } from "react-bootstrap";
 import "./admin.css";
 
-import { db } from "../../firebase";
+// import AddUserForm from "../../components/AddUser";
+// import AddAdminForm from "../../components/AddAdmin";
+
+import { db, adminAddUser, adminAddAdmin } from "../../firebase";
 import { collection, onSnapshot, where, query } from "firebase/firestore";   
 
 export default function AdminManagement() {
     const [userData, setUserData] = useState([]);
+    const [adminData, setAdminData] = useState([]);
+
+    const [showAddForm, setShowAddForm] = useState(false);
+    const handleShowAddForm = () => setShowAddForm(true);
+    const handleHideAddForm = () => {
+        setShowAddForm(false);
+    }
+
+    const [showAddAdminForm, setShowAddAdminForm] = useState(false);
+    const handleShowAddAdminForm = () => setShowAddAdminForm(true);
+    const handleHideAddAdminForm = () => {
+        // e.preventDefault();
+        setShowAddAdminForm(false);
+    }
     
     useEffect(()=>{
         if (collection){
@@ -24,9 +41,23 @@ export default function AdminManagement() {
             return fetchUserData;
         }
     }, [])
-    const test = () => {
-        console.log(userData)
-    }
+
+    useEffect(()=>{
+        if (collection){
+            const fetchAdminData = () => {
+                const q = query(collection(db, "users"), where("role", "==", 'Admin'))
+                onSnapshot(q,(querySnapshot)=>{
+                    const getAdminDataFromFirestore = [];
+                    querySnapshot.forEach((doc) => {
+                        console.log(doc.id, " => ", doc.data());
+                        getAdminDataFromFirestore.push({...doc.data(), key: doc.id})
+                    });
+                    setAdminData(getAdminDataFromFirestore);
+                });
+            }
+            return fetchAdminData;
+        }
+    }, [])
     
     return (
         <>
@@ -39,10 +70,36 @@ export default function AdminManagement() {
                 fill
             >
                 <Tab eventKey="admins" title="Admins account">
-                    <Button onClick={test}>Test</Button>
+                    <Container className="tabTitle">Admin accounts management</Container>
+                    <Button onClick={handleShowAddAdminForm}>Add Admin account</Button>
+                    <AddAdminForm show={showAddAdminForm} onHide={() => setShowAddAdminForm(false)}/>
+                    <Table  striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Provider</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                adminData.map(doc =>{
+                                    return(
+                                        <tr>
+                                            <th>{doc.username}</th>
+                                            <th>{doc.email}</th>
+                                            <th>{doc.authProvider}</th>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </Table> 
                 </Tab>
                 <Tab eventKey="users" title="Users account">
-                    <Button variant="primary">Add Users</Button>
+                    <Container className="tabTitle">User accounts management</Container>
+                    <Button variant="primary" onClick={handleShowAddForm}>Add Users</Button>
+                    <AddUserForm show={showAddForm} onHide={() => setShowAddForm(false)}/>
                     <Table  striped bordered hover>
                         <thead>
                             <tr>
@@ -71,29 +128,158 @@ export default function AdminManagement() {
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
+                            <th>Book title</th>
+                            <th>Author</th>
+                            <th>Day of public</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                    </tbody>
+                    <tbody></tbody>
                     </Table> 
                 </Tab>
             </Tabs>
         </Container>
+        
         </>
     )
+    function AddUserForm(props) {
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+        const [username, setUsername] = useState("");
+        const addUser = () => {
+            adminAddUser(username, email, password);
+            handleHideAddForm();
+        };
+        
+        
+        return(
+            <>
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                // backdrop="static"
+                className="addUserModal"
+            >
+                <Modal.Header>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Create User Account
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Container className="formWrapper">
+                        <Form className="adduserForm">
+                            <Form.Group className="mb-3" >
+                                <Form.Label>Username:</Form.Label>
+                                <Form.Control 
+                                    type="username" 
+                                    size="lg" 
+                                    id="username"
+                                    value={username}
+                                    placeholder="Enter your Username"
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" >
+                                <Form.Label>Email:</Form.Label>
+                                <Form.Control 
+                                    type="email" 
+                                    size="lg" 
+                                    id="email" 
+                                    value={email}
+                                    placeholder="Enter your Email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" >
+                                <Form.Label>Password:</Form.Label>
+                                <Form.Control 
+                                    type="password" 
+                                    size="lg" 
+                                    id="password"
+                                    value={password}
+                                    placeholder="Enter Password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Button variant="primary" size="lg" onClick={addUser}>Add Account</Button>
+                        </Form>
+                    </Container>
+                </Modal.Body>
+            </Modal>
+            </>
+        )
+    }
+
+    function AddAdminForm(props) {
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+        const [username, setUsername] = useState("");
+        const addAdmin = () => {
+            adminAddAdmin(username, email, password);
+            handleHideAddAdminForm();
+        };
+        return(
+            <>
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                // backdrop="static"
+                className="addAdminModal"
+            >
+                <Modal.Header>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Create Admin Account
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Container className="formWrapper">
+                        <Form className="adduserForm">
+                            <Form.Group className="mb-3" >
+                                <Form.Label>Username:</Form.Label>
+                                <Form.Control 
+                                    type="username" 
+                                    size="lg" 
+                                    id="username"
+                                    value={username}
+                                    placeholder="Enter your Username"
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" >
+                                <Form.Label>Email:</Form.Label>
+                                <Form.Control 
+                                    type="email" 
+                                    size="lg" 
+                                    id="email" 
+                                    value={email}
+                                    placeholder="Enter your Email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" >
+                                <Form.Label>Password:</Form.Label>
+                                <Form.Control 
+                                    type="password" 
+                                    size="lg" 
+                                    id="password"
+                                    value={password}
+                                    placeholder="Enter Password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Button variant="primary" size="lg" onClick={addAdmin}>Add Account</Button>
+                        </Form>
+                    </Container>
+                </Modal.Body>
+            </Modal>
+            </>
+        )
+    }
 }
+
+
+
