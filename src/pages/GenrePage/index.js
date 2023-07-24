c2fad2a9a3b6bb522f3a79cf0a5b417f351d771d
Le Dinh Khanh <nagadark111@gmail.com>
2023-07-24 08:40:42 +0700

    Responsive Genre Page

--- src/pages/GenrePage/index.js
+++ src/pages/GenrePage/index.js
@@ -1,86 +1,106 @@
-import React, {useState, useEffect} from 'react'
-import { Button, Container, Card } from 'react-bootstrap';
-import { Link, useParams } from "react-router-dom"
-import './genrepage.css'
+import React, { useState, useEffect } from "react";
+import { Button, Container, Card } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import "./genrepage.css";
 
-import { db } from '../../firebase';
+import { db } from "../../firebase";
 import { collection, onSnapshot, query, where } from "firebase/firestore";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 
 import useTable from "../../components/Pagination/useTable";
 import TableFooter from "../../components/Pagination/TableFooter";
 
 function GenrePage(props) {
-    const { handleAddToCart } = props;
-    const params = useParams();
-    const [bookDataFromGenre, setBookDataFromGenre] = useState([]);
-    const fetchBookDataFromGenre = () =>{
-        const q = query(collection(db, "books"), where("genre", "array-contains", params.genre ))
-        onSnapshot(q,(querySnapshot)=>{
-            setBookDataFromGenre(
-                querySnapshot.docs.map((doc)=>({
-                    id: doc.id,
-                    data: doc.data(),
-                }))
-            );
-        });
-    }
-    
-    useEffect(() => {
-        if(collection){
-            fetchBookDataFromGenre();
-        }
-    }, [params.genre])
+  const { handleAddToCart } = props;
+  const params = useParams();
+  const [bookDataFromGenre, setBookDataFromGenre] = useState([]);
+  const fetchBookDataFromGenre = () => {
+    const q = query(
+      collection(db, "books"),
+      where("genre", "array-contains", params.genre)
+    );
+    onSnapshot(q, (querySnapshot) => {
+      setBookDataFromGenre(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  };
 
-    let rowsPerPage = 10
-    const [page, setPage] = useState(1);
-    const { slice, range } = useTable(bookDataFromGenre, page, rowsPerPage)
-    
-    return (
-        <>
-        <Container className="genrePageWrapper">
-            <Container className="genrePageContainer">
-                <Container className="genrePageTitleContainer">
-                    <Container className="genrePageTitleTextContainer">Genre:&nbsp;{params.genre}</Container>
-                </Container>
-            </Container>
-            <Container className="genrePageBookContainer">
-                {slice?.map((el)=>{
-                    return(
-                        <Card key={el.id} style={{marginTop:"1vh"}}>
-                            <Link to={`/books/${el.id}`}>
-                                <Container className='allBookImageContainer'>
-                                    <Card.Img variant='top' src={el.data.image.url} style={{ width: '190px' }}/>
-                                </Container>
-                            </Link>
-                            <Card.Body>
-                                <Card.Title>{el.data.title}</Card.Title>
-                                <Card.Subtitle>{el.data.author}</Card.Subtitle>
-                                <Card.Text className='genrePageBookPrice'>{Number(el.data.price).toLocaleString("en-US",)} VND</Card.Text>
-                                <Card.Text style={{display:'flex'}}>
-                                    Genre:
-                                    <Container className="genrePageTagContainer">{params.genre}</Container>
-                                    <Container className="genrePageTagContainer">...</Container>
-                                </Card.Text>
-                                <Button 
-                                    className='genrePageAddCartBtn' 
-                                    onClick={()=>handleAddToCart(el.id, el.data)}
-                                    disabled={(el.data.stock === "0") ? true : false}
-                                >
-                                    <FontAwesomeIcon icon={faCartPlus}/>
-                                    &nbsp;Add to Cart
-                                </Button>
-                            </Card.Body>
-                        </Card>
-                    )
-                })}
-            </Container>
-            {bookDataFromGenre.length>10 && <TableFooter range={range} slice={slice} setPage={setPage} page={page}/>}
+  useEffect(() => {
+    if (collection) {
+      fetchBookDataFromGenre();
+    }
+  }, [params.genre]);
+
+  let rowsPerPage = 10;
+  const [page, setPage] = useState(1);
+  const { slice, range } = useTable(bookDataFromGenre, page, rowsPerPage);
+
+  return (
+    <>
+      <Container className="genrePageWrapper">
+        <Container className="genrePageContainer">
+          <Container className="genrePageTitleContainer">
+            Genre:&nbsp;{params.genre}
+          </Container>
         </Container>
-        </>
-    )
+        <Container className="genrePageBookContainer">
+          {slice?.map((el) => {
+            return (
+              <Card key={el.id} style={{ marginTop: "1vh" }}>
+                <Link to={`/books/${el.id}`}>
+                  <Container className="allBookImageContainer">
+                    <Card.Img variant="top" src={el.data.image.url} />
+                  </Container>
+                </Link>
+                <Card.Body>
+                  <Card.Title className="genrePageCardTitle">
+                    {el.data.title}
+                  </Card.Title>
+                  <Card.Subtitle className="genrePageCardSubTitle">
+                    {el.data.author}
+                  </Card.Subtitle>
+                  <Card.Text className="genrePageBookPrice">
+                    {Number(el.data.price).toLocaleString("en-US")} VND
+                  </Card.Text>
+                  <Card.Text style={{ display: "flex" }}>
+                    Genre:
+                    <Container className="genrePageTagContainer">
+                      {params.genre}
+                    </Container>
+                    <Container className="genrePageTagContainerEtc">
+                      ...
+                    </Container>
+                  </Card.Text>
+                  <Button
+                    className="genrePageAddCartBtn"
+                    onClick={() => handleAddToCart(el.id, el.data)}
+                    disabled={el.data.stock === "0" ? true : false}
+                  >
+                    <FontAwesomeIcon icon={faCartPlus} />
+                    &nbsp;Add to Cart
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Container>
+        {bookDataFromGenre.length > 10 && (
+          <TableFooter
+            range={range}
+            slice={slice}
+            setPage={setPage}
+            page={page}
+          />
+        )}
+      </Container>
+    </>
+  );
 }
 
-export default GenrePage
+export default GenrePage;
