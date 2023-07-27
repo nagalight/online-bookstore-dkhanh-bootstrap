7bc546c4e2f42d904f0eac097a93c09ec585acf1
Le Dinh Khanh <nagadark111@gmail.com>
2023-07-27 08:27:04 +0700

    Add logic to show between webview navbar with the responsive one

--- src/App.js
+++ src/App.js
@@ -1,121 +1,202 @@
-import './App.css';
+import "./App.css";
 import React, { useState, useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
-import NavigationBar from './components/NavigationBar/Web';
-import Footer from './components/Footer';
+import NavigationBar from "./components/NavigationBar/Web";
+import NavigationBarMobile from "./components/NavigationBar/Mobile";
+import Footer from "./components/Footer";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import AdminManagement from './pages/Admin';
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
-import InconstructionPage from './pages/InconstructionPage';
-import BookDetailPage from './pages/BookDetailPage';
-import AllBook from './pages/AllBook';
-import GenrePage from './pages/GenrePage';
-import SearchResult from './pages/SearchResult';
-import PaymentPage from './pages/PaymentPage';
-import NavigationBarMobile from './components/NavigationBar/Mobile';
+import AdminManagement from "./pages/Admin";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import InconstructionPage from "./pages/InconstructionPage";
+import BookDetailPage from "./pages/BookDetailPage";
+import AllBook from "./pages/AllBook";
+import GenrePage from "./pages/GenrePage";
+import SearchResult from "./pages/SearchResult";
+import PaymentPage from "./pages/PaymentPage";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
-  const handleLoading = () =>{
+  const handleLoading = () => {
     setTimeout(() => {
-      setIsLoading(false)
+      setIsLoading(false);
     }, 1000);
-  }
+  };
 
   useEffect(() => {
     if (document.referrer === "") {
-      return(
-        handleLoading()
-      )
-    }else{
-      setIsLoading(false)
+      return handleLoading();
+    } else {
+      setIsLoading(false);
     }
-  }, [])
-  
+  }, []);
 
   const [cartItems, setCartItems] = useState(
-    !window.sessionStorage.getItem('ZA_LIBRARY_CART_ITEM') ? 
-      [] : JSON.parse(window.sessionStorage.getItem('ZA_LIBRARY_CART_ITEM'))
+    !window.sessionStorage.getItem("ZA_LIBRARY_CART_ITEM")
+      ? []
+      : JSON.parse(window.sessionStorage.getItem("ZA_LIBRARY_CART_ITEM"))
   );
 
-  const handleAddToCart = (id, data) =>{
+  const handleAddToCart = (id, data) => {
     const bookExist = cartItems.find((item) => item.id === id);
-    if (bookExist){
-      setCartItems(cartItems.map((item)=> item.id === id ? {...bookExist, quantity:bookExist.quantity + 1} : item));
-    }else{
-      setCartItems([...cartItems,{...{id, data}, quantity: 1}])
+    if (bookExist) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === id
+            ? { ...bookExist, quantity: bookExist.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...{ id, data }, quantity: 1 }]);
     }
-  }
-  const handleRemoveFromCart = (id) =>{
+  };
+  const handleRemoveFromCart = (id) => {
     const bookExist = cartItems.find((item) => item.id === id);
-    if (bookExist.quantity === 1){
+    if (bookExist.quantity === 1) {
       setCartItems(cartItems.filter((item) => item.id !== id));
-    }else{
-      setCartItems(cartItems.map((item)=> item.id === id ? {...bookExist, quantity:bookExist.quantity - 1} : item))
+    } else {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === id
+            ? { ...bookExist, quantity: bookExist.quantity - 1 }
+            : item
+        )
+      );
     }
-  }
-  
-  const clearCart = () =>{
-    setCartItems([])
-  }
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
 
   useEffect(() => {
-    window.sessionStorage.setItem('ZA_LIBRARY_CART_ITEM', JSON.stringify(cartItems))
-  }, [cartItems])
+    window.sessionStorage.setItem(
+      "ZA_LIBRARY_CART_ITEM",
+      JSON.stringify(cartItems)
+    );
+  }, [cartItems]);
 
   useEffect(() => {
-    const cartData = window.sessionStorage.getItem('ZA_LIBRARY_CART_ITEM');
-    if (cartData !== null){
-      setCartItems(JSON.parse(cartData))
+    const cartData = window.sessionStorage.getItem("ZA_LIBRARY_CART_ITEM");
+    if (cartData !== null) {
+      setCartItems(JSON.parse(cartData));
     }
-  }, [])
-  
-  
+  }, []);
 
-  return (
-    !isLoading ? (
-      <>
+  const [windowSize, setWindowSize] = useState([window.innerWidth]);
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth]);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const [showNavBarWebView, setShowNavBarWebView] = useState("none");
+  const [showNavBarResponsive, setShowNavBarResponsive] = useState("none");
+
+  useEffect(() => {
+    if (windowSize[0] > 768) {
+      setShowNavBarWebView("block");
+      setShowNavBarResponsive("none");
+    } else if (windowSize[0] <= 768) {
+      setShowNavBarWebView("none");
+      setShowNavBarResponsive("block");
+    }
+  }, [windowSize]);
+
+  return !isLoading ? (
+    <>
       <BrowserRouter>
-        {/* <NavigationBar 
-          cartItems={cartItems} 
-          handleAddToCart={handleAddToCart} 
-          handleRemoveFromCart={handleRemoveFromCart}
-          clearCart={clearCart}
-        /> */}
-        <NavigationBarMobile
-          cartItems={cartItems} 
-          handleAddToCart={handleAddToCart} 
-          handleRemoveFromCart={handleRemoveFromCart}
-          clearCart={clearCart}
-        />
+        <Container
+          style={{
+            margin: 0,
+            padding: 0,
+            border: 0,
+            minWidth: "100%",
+            display: showNavBarWebView,
+          }}
+        >
+          <NavigationBar
+            cartItems={cartItems}
+            handleAddToCart={handleAddToCart}
+            handleRemoveFromCart={handleRemoveFromCart}
+            clearCart={clearCart}
+          />
+        </Container>
+
+        <Container
+          style={{
+            margin: 0,
+            padding: 0,
+            border: 0,
+            minWidth: "100%",
+            display: showNavBarResponsive,
+          }}
+        >
+          <NavigationBarMobile
+            cartItems={cartItems}
+            handleAddToCart={handleAddToCart}
+            handleRemoveFromCart={handleRemoveFromCart}
+            clearCart={clearCart}
+          />
+        </Container>
+
         <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/homepage" element={<HomePage/>}/>
-          <Route path="/admin" element={<AdminManagement/>}/>
-          <Route path="/*" element={<NotFoundPage/>}/>
-          <Route path="/404" element={<NotFoundPage/>}/>
-          <Route path="/inconstruction" element={<InconstructionPage/>}/>
-          <Route path="/genres/:genre" element={<GenrePage handleAddToCart={handleAddToCart}/>}/>
-          <Route exact path="/books" element={<AllBook handleAddToCart={handleAddToCart}/>} />
-          <Route exact path="/books/:id" element={<BookDetailPage handleAddToCart={handleAddToCart}/>}/>
-          <Route path="/searchResult/:keyword" element={<SearchResult handleAddToCart={handleAddToCart}/>}/>
-          <Route path="/payment" element={<PaymentPage cartItems={cartItems} setCartItems={setCartItems}/>}/>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/admin" element={<AdminManagement />} />
+          <Route path="/*" element={<NotFoundPage />} />
+          <Route path="/404" element={<NotFoundPage />} />
+          <Route path="/inconstruction" element={<InconstructionPage />} />
+          <Route
+            path="/genres/:genre"
+            element={<GenrePage handleAddToCart={handleAddToCart} />}
+          />
+          <Route
+            exact
+            path="/books"
+            element={<AllBook handleAddToCart={handleAddToCart} />}
+          />
+          <Route
+            exact
+            path="/books/:id"
+            element={<BookDetailPage handleAddToCart={handleAddToCart} />}
+          />
+          <Route
+            path="/searchResult/:keyword"
+            element={<SearchResult handleAddToCart={handleAddToCart} />}
+          />
+          <Route
+            path="/payment"
+            element={
+              <PaymentPage cartItems={cartItems} setCartItems={setCartItems} />
+            }
+          />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
-      </>
-    ) : (
-      <div id='spinner' className='loadingWrapper'>
-        <img className='loadingImage' src="https://firebasestorage.googleapis.com/v0/b/za-library-account.appspot.com/o/Main%2FZA_icon.png?alt=media&token=d281410f-e149-46b4-bf1a-b3e9375f62fd" alt='Za_Icon' />
-        <div className='loading'></div>
-      </div>
-    )
+    </>
+  ) : (
+    <div id="spinner" className="loadingWrapper">
+      <img
+        className="loadingImage"
+        src="https://firebasestorage.googleapis.com/v0/b/za-library-account.appspot.com/o/Main%2FZA_icon.png?alt=media&token=d281410f-e149-46b4-bf1a-b3e9375f62fd"
+        alt="Za_Icon"
+      />
+      <div className="loading"></div>
+    </div>
   );
 }
 
